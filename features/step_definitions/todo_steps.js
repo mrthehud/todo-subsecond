68b9c91330c45422218be9acf6168d202157d676
James Hudson <james.hudson@capablue.com>
2017-10-12 09:21:38 +0100

    Edits from meetup

--- features/step_definitions/todo_steps.js
+++ features/step_definitions/todo_steps.js
@@ -19,3 +19,17 @@ Then('the text of the {ordinal} todo should be {string}', async function(index, 
   const todos = await todoList.getTodos()
   assert.equal(todos[index].text, text)
 })
+
+When('I mark the {ordinal} todo as done', async function (index) {
+    // Write code here that turns the phrase above into concrete actions
+    const todoList = await this.actionTodoList()
+    await todoList.markAsDone(index);
+});
+
+
+Then('the {ordinal} todo should be marked as done', async function (index) {
+    // Write code here that turns the phrase above into concrete actions
+    const todoList = await this.outcomeTodoList()
+    const todos = await todoList.getTodos()
+    assert.equal(todos[index].done, true)
+});
